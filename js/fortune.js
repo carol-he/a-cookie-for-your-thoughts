e9c1615713a80cb06a029e86bba51e66e3bac9db
Carol He <ch2727@nyu.edu>
2017-06-21 14:33:16 -0700

    design changes

--- js/fortune.js
+++ js/fortune.js
@@ -1,25 +1,73 @@
 var fortunes = [
-  {"cn": "", "eng": "Be so completely yourself that everyone else feels safe to be themselves too."},
-  {"cn": "你永远不会后悔善良", "eng": "You never regret being kind."},
-  {"cn": "", "eng": "Sometimes, you're the mess. Sometimes, you're the broom. On the hardest days, you'll have to be both.", "long": true},
-  {"cn": "", "eng": "Don’t trip over people who don’t value you."},
-  {"cn": "", "eng": "Stop being tormented by everyone else’s reaction of you."},
-  {"cn": "", "eng": "Sometimes self care is cracking open a cold one without the boys."},
-  {"cn": "", "eng": "Be with somebody who boosts your confidence, not makes you question it."},
-  {"cn": "你怀念过去的你", "eng": "You miss yourself."},
-  {"cn": "", "eng": "No one warns you about the mourning in growth."},
-  {"cn": "你赢或你成长", "eng": "You win or you learn."},
-  {"cn": "", "eng": "It is lovely to talk to a person and hear their voice smile."},
-  {"cn": "", "eng": "Choose people who choose you."},
-  {"cn": "", "eng": "We never know which lives we influence, when, or why."},
-  {"cn": "", "eng": "Not all who mcfreakin wander are mcfreakin lost"},
-  {"cn": "", "eng": "It's okay that you're not who you thought you would be."}
+  {
+    "cn": "",
+    "eng": "Be so completely yourself that everyone else feels safe to be themselves, too."
+  },
+  {
+    "cn": "你永远不会后悔善良",
+    "eng": "You never regret being kind."
+  },
+  {
+    "cn": "",
+    "eng": "Sometimes, you're the mess. Sometimes, you're the broom. On the hardest days, you'll have to be both."
+  },
+  {
+    "cn": "",
+    "eng": "Don’t trip over people who don’t value you."
+  },
+  {
+    "cn": "",
+    "eng": "Stop being tormented by everyone else’s reaction of you."
+  },
+  {
+    "cn": "",
+    "eng": "Sometimes self care is cracking open a cold one without the boys."
+  },
+  {
+    "cn": "",
+    "eng": "Be with somebody who boosts your confidence, not makes you question it."
+  },
+  {
+    "cn": "你怀念过去的你",
+    "eng": "You miss yourself."
+  },
+  {
+    "cn": "",
+    "eng": "No one warns you about the mourning in growth."
+  },
+  {
+    "cn": "你赢或你成长", "eng": "You win or you learn."
+  },
+  {
+    "cn": "",
+    "eng": "It is lovely to talk to a person and hear their voice smile."
+  },
+  {
+    "cn": "",
+    "eng": "Choose people who choose you."
+  },
+  {
+    "cn": "",
+    "eng": "We never know which lives we influence, when, or why."
+  },
+  {
+    "cn": "",
+    "eng": "Not all who mcfreakin wander are mcfreakin lost"
+  },
+  {
+    "cn": "",
+    "eng": "It's okay that you're not who you thought you would be."
+  }
 ];
 document.querySelector('#cookie').addEventListener('click', openCookie);
 function openCookie(){
   var fortune = document.createElement('div');
   fortune.className = 'fortune';
   fortune.textContent = fortunes[fortunes.length * Math.random() << 0].eng;
+  if(fortune.textContent.length > 75){
+    fortune.style.fontSize = '90%';
+    fortune.style.padding = '5px 10px';
+  }
   var topright = document.createElement('div');
   topright.className = 'topright';
   fortune.appendChild(topright);
